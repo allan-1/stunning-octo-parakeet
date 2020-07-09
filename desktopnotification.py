4b2786162464957e3b5b2b58ee0bb536c6c537e4
Allan Muturi Maina <amuturi01@gmail.com>
2020-07-09 09:17:46 +0300

    desktopnotification.py

---
+++ desktopnotification.py
@@ -0,0 +1,8 @@
+from plyer import notification
+notification.notify(
+    title = "This is my first test",
+    message = "I hope get right this time",
+    app_icon = None,
+    timeout = 10
+    
+    )
